53b33d469eade4e14c14a8e67cea4c39be8855c9
brendon <bhssilva@inf.ufpel.edu.br>
2019-04-13 02:24:36 -0300

    trying again

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var serveStatic = require('serve-static')
 var path = require('path')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, '/dist')))
 
-var port = process.env.PORT || 8000
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
